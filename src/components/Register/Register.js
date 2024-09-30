adba9d46c1392f3339aba123f36452710ef43ed6
Tan Pham <tann4878@gmail.com>
2024-10-01 01:05:22 +0700

    #10.1.1

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,19 +1,26 @@
 import './Register.scss';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const Register = (props) => {
     let history = useHistory();
     
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
     const handleLogin = () => {
         history.push('/login');
     }
 
+    const handleRegister = () => {
+        let userData = { email, phone, username, password };
+    }
+
     useEffect(() => {
-        axios.get('http://localhost:8080/api/test-api').then(data => {
-            console.log(">>> check data axios: ", data)
-        })
     }, [])
 
     return (
@@ -28,25 +35,35 @@ const Register = (props) => {
                         <div className='brand d-sm-none'> ABC </div>
                         <div className='form-group'>
                             <label> Email: </label>
-                            <input type='text' className='form-control' placeholder='Email' />
+                            <input type='text' className='form-control' placeholder='Email' 
+                                value={email} onChange={(e) => setEmail(e.target.value)}
+                            />
                         </div>
                         <div className='form-group'>
                             <label> Phone number: </label>
-                            <input type='text' className='form-control' placeholder='Phone number' />
+                            <input type='text' className='form-control' placeholder='Phone number' 
+                                value={phone} onChange={(e) => setPhone(e.target.value)}
+                            />
                         </div>
                         <div className='form-group'>
                             <label> Username: </label>
-                            <input type='text' className='form-control' placeholder='Username' />
+                            <input type='text' className='form-control' placeholder='Username' 
+                                value={username} onChange={(e) => setUsername(e.target.value)}
+                            />
                         </div>
                         <div className='form-group'>
                             <label> Password: </label>
-                            <input type='password' className='form-control' placeholder='Password' />
+                            <input type='password' className='form-control' placeholder='Password' 
+                                value={password} onChange={(e) => setPassword(e.target.value)}
+                            />
                         </div>
                         <div className='form-group'>
                             <label> Re-enter password: </label>
-                            <input type='password' className='form-control' placeholder='Re-enter password' />
+                            <input type='password' className='form-control' placeholder='Re-enter password' 
+                                value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}
+                            />
                         </div>
-                        <button className='btn btn-primary'> Register </button>
+                        <button className='btn btn-primary' onClick={() => handleRegister()}> Register </button>
                         <hr />
                         <div className='text-center'>
                             <button className='btn btn-success' onClick={() => handleLogin()}> Already've an account. Login </button>
