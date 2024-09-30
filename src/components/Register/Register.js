0bb099e99f369a436c39ee0646d984f427871fee
Tan Pham <tann4878@gmail.com>
2024-10-01 04:44:52 +0700

    #10.2.2

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,8 +1,8 @@
 import './Register.scss';
 import { useHistory } from 'react-router-dom';
-import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
+import { registerNewUser } from '../../services/userService';
 
 const Register = (props) => {
     let history = useHistory();
@@ -56,10 +56,17 @@ const Register = (props) => {
         return true;
     }
 
-    const handleRegister = () => {
+    const handleRegister = async () => {
         let check = isValidInputs();
         if(check === true){
-            axios.post('http://localhost:8080/api/v1/register', {email, phone, username, password})
+            let response = await registerNewUser(email, phone, username, password);
+            let serverData = response.data;
+            if(+serverData.EC === 0){
+                toast.success(serverData.EM);
+                history.push('/login');
+            } else{
+                toast.error(serverData.EM);
+            }
         }
     }
 
