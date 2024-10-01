a279abf454c98c6a866d3568528a17247759be7c
Tan Pham <tann4878@gmail.com>
2024-10-01 19:13:54 +0700

    #10.6.1

--- src/services/userService.js
+++ src/services/userService.js
@@ -8,7 +8,12 @@ const loginUser = (valueLogin, password) => {
     return axios.post('http://localhost:8080/api/v1/login', {valueLogin, password})
 }
 
+const fetchAllUser = () => {
+    return axios.get('http://localhost:8080/api/v1/user/read')
+}
+
 export {
     registerNewUser,
-    loginUser
+    loginUser,
+    fetchAllUser
 }
