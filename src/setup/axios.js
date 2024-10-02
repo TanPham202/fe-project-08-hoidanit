3e1dadac8fd5437ff09a4f555115a1a169604033
Tan Pham <tann4878@gmail.com>
2024-10-03 00:47:37 +0700

    #11.5

--- src/setup/axios.js
+++ src/setup/axios.js
@@ -4,6 +4,8 @@ const instance = axios.create({
     baseURL: 'http://localhost:8080'
 })
 
+instance.defaults.withCredentials = true;
+
 instance.interceptors.request.use(function (config){
     return config;
 }, function(error){
